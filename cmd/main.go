d9f68053ed6e929000e55b23cf33cf45affce514
Yuriy Taranov <ig.seldom@gmail.com>
2021-05-02 12:56:55 +0300

    added godotenv

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/joho/godotenv"
+	"github.com/yuriytaranov/youtube-stats/internal/pkg/websocket"
 )
 
 // homePage will be a simple "hello World" style page
@@ -11,17 +14,38 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World")
 }
 
+// our new stats function which will expose any YouTube
+// stats via a websocket connection
+func stats(w http.ResponseWriter, r *http.Request) {
+	// we call our new websocket package Upgrade
+	// function in order to upgrade the connection
+	// from a standard HTTP connection to a websocket one
+	ws, err := websocket.Upgrade(w, r)
+	if err != nil {
+		fmt.Println(w, "%+v\n", err)
+	}
+	// we the call our Writer function
+	// which continually polls and writes the results
+	// to this websocket connection
+	go websocket.Writer(ws)
+}
+
 // setupRoutes handles setting up our servers
 // routes and matching them to their respective
 // functions
 func setupRoutes() {
 	http.HandleFunc("/", homePage)
+	http.HandleFunc("/stats", stats)
 	// here we kick off our server on localhost:8080
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 // our main function
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	fmt.Println("YouTube Subscriber Monitor")
 	// call setup routes
 	setupRoutes()
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/yuriytaranov/youtube-stats
 
 go 1.16
 
-require github.com/gorilla/websocket v1.4.2 // indirect
+require (
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/joho/godotenv v1.3.0 // indirect
+)
